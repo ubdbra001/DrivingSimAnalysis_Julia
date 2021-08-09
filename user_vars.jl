bb43bdf987da481123b6d21239c052c73b5d84cd
ubdbra001 <dan.brady@rocketmail.com>
2021-08-09 18:51:24 +0100

    Added user_vars file

---
+++ user_vars.jl
@@ -0,0 +1,13 @@
+target_object = Dict(
+    "name" => "roadworks1",
+    "created_point" => 5000,
+    "start_point" => 6000,
+    "end_point" => 4000,
+    "velocity_fps" => 30,
+    "sample_rate" => 30,
+ )
+
+ window_properties = Dict(
+    "time_range_s" => [1,1],
+    "distance_range_ft" => [],
+ )  
