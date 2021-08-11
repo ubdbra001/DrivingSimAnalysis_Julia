3ae5d2b45a9c0b8fa19bce6b6143cf02049394f7
ubdbra001 <dan.brady@rocketmail.com>
2021-08-11 12:41:05 +0100

    Added new properties to the user_vars file

--- user_vars.jl
+++ user_vars.jl
@@ -1,9 +1,9 @@
-target_object = Dict(
+nonvehicle_target = Dict(
     "name" => "roadworks1",
-    "created_point" => 5000,
-    "start_point" => 6000,
+    "created_point" => 3500,
+    "dist_from_driver" => 3000,
     "end_point" => 4000,
-    "velocity_fps" => 30,
+    "velocity_fps" => 0,
     "sample_rate" => 30,
  )
 
@@ -11,3 +11,16 @@ target_object = Dict(
     "time_range_s" => [1,1],
     "distance_range_ft" => [],
  )  
+
+ directories = Dict(
+     "raw_data" => "data/raw_data/",
+     "extracted_driver" => "data/extracted_data/driver_data/",
+     "extracted_other" => "data/extracted_data/other_vehicle_data/"
+ )
+
+ filenames = Dict(
+     "extracted_driver" => "p(placeholder)_driverdata.csv",
+     "extracted_other" => "p(placeholder)_othervehicledata.csv"
+ )
+
+ 
