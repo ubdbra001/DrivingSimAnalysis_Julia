014d0b48db99d892068f7629fc9ab96ced732d39
ubdbra001 <dan.brady@rocketmail.com>
2021-08-11 20:49:46 +0100

    Init commit of download data script

---
+++ 00_get_data_from_osf.jl
@@ -0,0 +1,7 @@
+# URL for data from OSF
+url = "https://files.de-1.osf.io/v1/resources/frv29/providers/osfstorage/5fd8d12c0694b703e2f36c69/?zip="
+
+# Not sure how to get this to save to a file
+download(url)
+
+# Once downloaded extract to raw_data dir using zipfile package 
